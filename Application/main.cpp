bcc1596df78e0ace08dbb64d98d2c542f24d0d93
Y. Onoue <y-onoue@findix.co.jp>
2022-08-01 14:21:49 +0900

    jsonファイルの読み込み

--- Application/main.cpp
+++ Application/main.cpp
@@ -6,7 +6,17 @@
 #include "viewer.h"
 
 using namespace std;
+using json = nlohmann::json;
 
 int main() {
+	FILE* fp;
+
+	fopen_s(&fp, "../config/setting.json", "r");
+	auto j = json::parse(fp);
+	fclose(fp);
+
+	cout << "bufferSize: " << j["bufferSize"] << endl;
+	cout << "fps: " << j["fps"] << endl;
+
 	return 0;
 }
