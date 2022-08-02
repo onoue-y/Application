3283ff08b8d0201cdeecf0393cdb64cc4dfe23d6
Y. Onoue <y-onoue@findix.co.jp>
2022-08-02 16:37:20 +0900

    viewerクラスの作成

---
+++ Application/viewer.cpp
@@ -0,0 +1,37 @@
+#include <opencv2/opencv.hpp>
+#include <queue>
+#include <vector>
+#include "viewer.h"
+#include "ringBuffer.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world455d.lib")
+#else
+#pragma comment(lib, "opencv_world455.lib")
+#endif
+
+using namespace std;
+using namespace cv;
+
+void Viewer::view(RingBuffer* ringBuffer, queue<int>* message2) {
+	while (1) {
+		if (!(message2->empty())) {
+			messageNum = message2->front();
+			switch (messageNum) {
+			case 0:
+				break;
+			case 1:
+				ringBuffer->Get(&frame, &contour);
+				rectangle(frame, Point(contour.x, contour.y), Point(contour.x + contour.width, contour.y + contour.height), Scalar(0, 0, 255), 3);
+				break;
+			case 2:
+				break;
+			default:
+				break;
+			}
+			message2->pop();
+		}
+		const int key = waitKey(1);
+		if (key == 27) break; //キーコード [Esc]:27
+	}
+}
---
+++ Application/viewer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <queue>
+#include <vector>
+#include "ringBuffer.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world455d.lib")
+#else
+#pragma comment(lib, "opencv_world455.lib")
+#endif
+
+using namespace std;
+using namespace cv;
+
+class Viewer {
+private:
+	Mat frame;
+	Rect contour;
+	int messageNum;
+public:
+	void view(RingBuffer* ringBuffer, queue<int>* message2);
+};
