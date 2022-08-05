a05e03818aeb306ffeb9f5df06c8de747ea524f1
Y. Onoue <y-onoue@findix.co.jp>
2022-08-05 17:52:25 +0900

    constants.hの追加

---
+++ Application/constants.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world455d.lib")
+#else
+#pragma comment(lib, "opencv_world455.lib")
+#endif
+
+using namespace cv;
+
+const int openError = -1;
+const Rect notDetect = { 0,0,0,0 };
+const int esc = 27;
+const int escMessage = 2;
+const int getMessage = 1;
+const int blue = 0;
+const int green = 0;
+const int red = 255;
+const int thickness = 3;
+const int waitSecond = 1;
+const double scaleFactor = 1.1;
+const int minNeighbors = 3;
+const int flags = 0;
+const int minsize = 30;
